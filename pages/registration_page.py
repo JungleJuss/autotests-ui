da2fab8af17510673dac6d678347b286d3ae1347
JungleJuss <kosmax23@mail.ru>
2025-09-10 01:23:26 +0300

    Практикуемся в реализации PageComponent. Практикуемся в написании компонентов с использованием паттерна PageComponent.

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,4 +1,6 @@
 from playwright.sync_api import expect, Page
+
+from components.authentification.registration_form_component import RegistrationFormComponent
 from pages.base_page import BasePage
 
 
@@ -6,21 +8,10 @@ class RegistrationPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.email_input = page.get_by_test_id('registration-form-email-input').locator("input")
-        self.username_input = page.get_by_test_id('registration-form-username-input').locator("input")
-        self.pasword_input = page.get_by_test_id('registration-form-password-input').locator("input")
+        self.registration_form = RegistrationFormComponent(page)
+
         self.registration_button = page.get_by_test_id('registration-page-registration-button')
         self.login_button = page.get_by_test_id('registration-page-login-link')
-
-    def fill_registration_form(self, email: str, username: str, password: str):
-        self.email_input.fill(email)
-        expect(self.email_input).to_have_value(email)
-
-        self.username_input.fill(username)
-        expect(self.username_input).to_have_value(username)
-
-        self.pasword_input.fill(password)
-        expect(self.pasword_input).to_have_value(password)
 
     def click_registration_button(self):
         self.registration_button.click()
